45ad5cb482c267e3e005d425551f2a6bc35454c8
m1rade <forx98@gmail.com>
2022-07-20 16:35:22 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import TodoList, {TaskType} from "./TodoList";
+
+function App() {
+    const tasks: Array<TaskType> = [
+        {id: 0, title: "HTML", isDone: true},
+        {id: 1, title: "CSS", isDone: true},
+        {id: 2, title: "JS/TS", isDone: false},
+    ];
+    return (
+        <>
+            <TodoList title={"What to learn"} tasks={tasks}/>
+        </>
+    );
+}
+
+export default App;
