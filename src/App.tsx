a15808ebd55a2e67a2b973f9ffa87f62fa3dcb62
m1rade <forx98@gmail.com>
2022-07-25 19:40:17 +0300

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import TodoList, {TaskType} from "./TodoList";
 
 function App() {
-    let tasks: Array<TaskType> = [
+    const [tasks, setTasks] = useState<Array<TaskType>>([
         {id: 1, title: "HTML", isDone: true},
         {id: 2, title: "CSS", isDone: true},
         {id: 3, title: "JS/TS", isDone: false},
-
-    ];
+    ]);
 
     const removeTask = (taskID: number) => {
         // получили новый массив, исключив элемент
-        tasks = tasks.filter(task => task.id !== taskID);
+        setTasks(tasks.filter(task => task.id !== taskID));
         console.log(tasks);
-    }
+    };
 
     return (
         <>
