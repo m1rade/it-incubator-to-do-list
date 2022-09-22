77c4a75073128a1fc95cba743249e519fe38d2e3
m1rade <forx98@gmail.com>
2022-09-22 17:59:12 +0300

    Fixed createStore

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,4 +1,4 @@
-import {combineReducers, createStore} from "redux";
+import {combineReducers, legacy_createStore} from "redux";
 import {todoLists_reducer} from "./todoLists_reducer";
 import {tasks_reducer} from "./tasks_reducer";
 
@@ -9,7 +9,7 @@ const rootReducer = combineReducers({
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
-export const store = createStore(rootReducer);
+export const store = legacy_createStore(rootReducer);
 
 // @ts-ignore
 window.store = store;
