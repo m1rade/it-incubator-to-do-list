b5a894136d611dbf1636c956321b2b9aba4d5645
m1rade <forx98@gmail.com>
2022-11-07 17:44:08 +0300

    Interaction with ServerAPI, Axios, TaskAPI

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -8,18 +8,11 @@ const instance = axios.create({
     },
 })
 
-// const settings = {
-//     withCredentials: true,
-//     headers: {
-//         "API-KEY": "c2f95e37-50c6-42af-975e-1d3b28d3998b"
-//     }
-// }
-
 export const todolistAPI = {
     updateTodolist(todolistId: string, title: string) {
         return instance.put<ResponseType>(
             `todo-lists/${todolistId}`,
-            {title: title}
+            {title}
         )
     },
     getTodolists() {
@@ -30,6 +23,18 @@ export const todolistAPI = {
     },
     deleteTodolist(todolistID: string) {
         return instance.delete<ResponseType>(`todo-lists/${todolistID}`);
+    },
+    createTask(todolistID: string, title: string) {
+        return instance.post<ResponseType<{ item: TaskType }>>(`todo-lists/${todolistID}/tasks`, {title});
+    },
+    getTasks(todolistID: string, count?: number, page?: number) {
+        return instance.get<ResponseType<{ items: TaskType[] }>>(`todo-lists/${todolistID}/tasks?count=${count}&page=${page}`);
+    },
+    deleteTask(todolistID: string, taskID: string) {
+        return instance.delete<ResponseType>(`todo-lists/${todolistID}/tasks/${taskID}`);
+    },
+    updateTask(todolistID: string, taskID: string, taskItem: UpdateTaskType) {
+        return instance.put<ResponseType<{ item: TaskType }>>(`todo-lists/${todolistID}/tasks/${taskID}`, taskItem);
     }
 }
 
@@ -40,25 +45,33 @@ type TodolistType = {
     order: number,
 }
 
+type TaskType = {
+    addedDate: string,
+    deadline: string | null,
+    description: string | null,
+    id: string,
+    order: number,
+    priority: number
+    startDate: string | null,
+    status: number,
+    title: string,
+    todoListId: string,
+    completed: boolean,
+}
+
+type UpdateTaskType = {
+    title: string,
+    description: string | null,
+    completed: boolean,
+    status: number,
+    priority: number,
+    startDate: string | null,
+    deadline: string | null,
+}
+
 type ResponseType<T = {}> = {
     data: T,
     messages: string[],
     fieldErrors: string[],
     resultCode: number,
 }
-
-// type CreateTodolistResponseType = {
-//     data: {
-//         item: TodolistType,
-//     }
-//     resultCode: number,
-//     messages: string[],
-//     fieldErrors: string[],
-// }
-//
-// type UpdateTodolistResponseType = {
-//     data: {},
-//     messages: string[],
-//     fieldErrors: string[],
-//     resultCode: number,
-// }
